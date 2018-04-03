93116e66d31c2033cb4d4c65c0bd8b53e9005926
anjuncc <anjuncc@163.com>
2018-04-04 01:12:16 +0800

    access_token

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 'use strict'
 var koa = require('koa')
 var sha1 = require('sha1')
+var path = require('path')
+var util = require('./libs/util')
 var wechat = require('./wechat/g')
+var wechat_file = path.join(__dirname, './config/wechat.txt')
 var config = {
     wechat: {
         appID: 'wx4c10dcbc112ceaeb',
         appSecret: '6f802e7cef82c74596b9760e3b31f4ff',
-        token: 'd027us7DzEzod7So0ddTE27KZyEUZoFo'
+        token: 'd027us7DzEzod7So0ddTE27KZyEUZoFo',
+        getAccessToken: function() {
+            return util.readFileAsync(wechat_file, 'utf8')
+        },
+        saveAccessToken: function(data) {
+            return util.writeFileAsync(wechat_file, data)
+        }
+
     }
 }
 var app = new koa()
